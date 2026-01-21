a6934c3c57ce7ac443381253c01ea5eddf3876e8
IbnuParvez <parvezfatma98@gmail.com>
2026-01-21 18:42:39 +0300

    Add Gamecontroller module skeleton

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ const Gameboard = (function () {
   };
   return { readBoard, placeMark, resetBoard };
 })();
+
+const Gamecontroller = (function () {
+  const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const playRound = function () {};
+
+  const getActivePlayer = function () {};
+
+  const restartGame = function () {};
+
+  return { playRound, getActivePlayer, restartGame };
+})();
