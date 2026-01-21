ea0ec4ca71e20a3f0b1180d4c75ce36cdd7cbc69
IbnuParvez <parvezfatma98@gmail.com>
2026-01-21 18:18:31 +0300

    Implement placeMark and resetBoard

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const Gameboard = (function () {
 
   const readBoard = () => board;
 
-  const resetBoard = () =>
+  const placeMark = function (index, mark) {
+    if (board[index] !== "") {
+      return false;
+    } else {
+      board[index] = mark;
+      return true;
+    }
+  };
+
+  const resetBoard = function () {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
+  };
   return { readBoard, placeMark, resetBoard };
 })();
